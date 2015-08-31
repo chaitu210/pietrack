a14df000bafc1f7676c49bd9eead8a66fb049ed6
druuu <druuu.mail@gmail.com>
2015-08-31 17:41:47 +0530

    add_task, cleaned

--- task/views.py
+++ task/views.py
@@ -9,7 +9,6 @@ def add_task(request, slug):
         add_task_dict= request.POST.copy()
         add_task_dict['project'] = project_obj.id
         try:
-            print add_task_dict
             ticket_name = request.POST.get('name')
             if Ticket.objects.filter(name = ticket_name).exists():
                 ticket_type = 'issue'
@@ -17,19 +16,10 @@ def add_task(request, slug):
                 ticket_type = 'enhancement'
             add_task_dict['ticket_type'] = ticket_type
         except:
-            print 'hi'
+            pass
         json_data = {}
         add_task_form = AddTaskForm(add_task_dict)
         if add_task_form.is_valid():
-            #json_data['error'] = False
-            #if Requirement.objects.filter(id = request.POST.get('requirement')).exists():
-            #    Requirement.objects.get(id = request.POST.get('requirement'))
-            #else:
-            #    reuirement_obj = None
-            #status_obj = TicketStatus.objects.get(id = request.POST.get('status'))
-            #assigned_to_obj = User.objects.get(id = request.POST.get('assigned_to'))
-            ##project_obj = requirement_obj.project
-            #Ticket.objects.create(name = ticket_name, requirement = requirement_obj, status = status_obj, assigned_to = assigned_to_obj, finished_date = request.POST.get('finished_date'), description = request.POST.get('description'), project = project_obj, ticket_type = ticket_type)
             add_task_form.save()
             return HttpResponse(json.dumps(json_data), content_type = 'application/json')
         else:
