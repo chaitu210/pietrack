f1c893ebbb5314ca06e4890f8147965beaf42151
Anjaneyulu Batta <anjaneyulu.batta@micropyramid.com>
2015-09-30 12:45:55 +0530

    import changed

--- project/receivers.py
+++ project/receivers.py
@@ -1,6 +1,8 @@
 from django.dispatch import receiver
-from signals import create_timeline
+from . import signals
 from piebase.models import Timeline
-@receiver(create_timeline)
+
+
+@receiver(signals.create_timeline)
 def timeline_activity(sender, **kwargs):
     Timeline.objects.create(user=sender, content_object=kwargs.get("content_object"),namespace=kwargs.get("namespace"),event_type=kwargs.get("event_type"), project=kwargs.get("project"))
